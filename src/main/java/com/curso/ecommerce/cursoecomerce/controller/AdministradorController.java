4565fa2e7406314fe4462eba1c70454b7e1c33f5
Brandon Lezama <lezaama55-@hotmail.com>
2023-02-27 21:36:03 -0600

    Crear las clases model, mapeo clases a tablas con JPA, integrado al diseño y vistas

---
+++ src/main/java/com/curso/ecommerce/cursoecomerce/controller/AdministradorController.java
@@ -0,0 +1,20 @@
+package com.curso.ecommerce.cursoecomerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//Estamos mapeando la cadena administrador para en el navegador encuentre la dirección 
+//del localhost principal
+@RequestMapping("/administrador")
+public class AdministradorController {
+
+    //Redirección para el localhost 8080 hacia a la home
+    @GetMapping("")
+    public String home(){
+        //Redirecciona al la vista principal
+        return "administrador/home";
+    }
+    
+}
